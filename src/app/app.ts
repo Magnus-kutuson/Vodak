c9a1cb8381f89eb189e6508d687097b2c32b6cc0
Magnus <abeikumagnus@gmail.com>
2026-04-26 23:03:53 +0100

    add stats component to app module and update template

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
 import { Hero } from './hero/hero';
+import { Stats } from './stats/stats';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Hero],
+  imports: [RouterOutlet, Header, Hero, Stats],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
